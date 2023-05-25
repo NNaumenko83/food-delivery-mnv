94372774ac7e69ec93dc6f08671cb626295a84b6
Nikolay Naumenko <nnv2006@ukr.net>
2023-05-25 09:52:10 +0300

    fix errors

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Button, Card } from "./ProductCard.styled";
-import { addProduct, selectProducts } from "../../redux/productsSlice";
-import { toast } from "react-toastify";
-import { addShop, selectShop } from "../../redux/shopSlice";
-import { useLocation } from "react-router-dom";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, Card } from './ProductCard.styled';
+import { addProduct, selectProducts } from '../../redux/productsSlice';
+import { toast } from 'react-toastify';
+import { addShop } from '../../redux/shopSlice';
+import { useLocation } from 'react-router-dom';
 
 export const ProductCard = ({ name, img, price, id }) => {
   const dispatch = useDispatch();
-  const shop = useSelector(selectShop);
+
   const location = useLocation();
 
   const selectedProducts = useSelector(selectProducts);
@@ -18,45 +18,45 @@ export const ProductCard = ({ name, img, price, id }) => {
       dispatch(addShop(location.pathname));
     }
 
-    const productArray = selectedProducts.filter((product) => {
+    const productArray = selectedProducts.filter(product => {
       return product.id === id;
     });
 
     if (productArray.length !== 0) {
-      toast.info("This item is already in the cart", {
-        position: "top-center",
+      toast.info('This item is already in the cart', {
+        position: 'top-center',
         autoClose: 2000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "colored",
+        theme: 'colored',
       });
       return;
     }
 
     dispatch(addProduct({ name, img, price, id }));
 
-    toast.success("Product is added", {
-      position: "top-center",
+    toast.success('Product is added', {
+      position: 'top-center',
       autoClose: 2000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
       progress: undefined,
-      theme: "light",
+      theme: 'light',
     });
   };
 
   return (
     <Card>
       <img
-        style={{ width: "100%", height: "auto", borderRadius: "10px" }}
+        style={{ width: '100%', height: 'auto', borderRadius: '10px' }}
         src={img}
         alt={name}
-        width={"300px"}
+        width={'300px'}
       />
       <h3>{name}</h3>
       <p>{price} UAH</p>
--- src/services/ShopAPI.js
+++ src/services/ShopAPI.js
@@ -1,11 +1,11 @@
-import mcDonaldsProducts from "../db/md.json";
-import kfcProducts from "../db/kfc.json";
-import murakamiProducts from "../db/murakami.json";
+import mcDonaldsProducts from '../db/md.json';
+// import kfcProducts from "../db/kfc.json";
+// import murakamiProducts from "../db/murakami.json";
 
 export const getProducts = () => {
   return mcDonaldsProducts;
 };
 
-export const getProductById = (productId) => {
-  return mcDonaldsProducts.find((product) => product.id === productId);
+export const getProductById = productId => {
+  return mcDonaldsProducts.find(product => product.id === productId);
 };
